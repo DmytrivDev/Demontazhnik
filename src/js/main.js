427f2261d306db5d5f7ef900e36015fea398928a
Uzurch1k <galcenkovlad7@gmail.com>
2024-09-26 23:28:22 +0300

    add done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+import 'img-comparison-slider/dist/index.js';
+import 'img-comparison-slider/dist/styles.css';
+
 import { initMenu } from './parts/navigation';
 import './parts/splide';
 import './parts/part';
