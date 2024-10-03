d265d8a48be44d60082cd5523dc3baca829df84c
Uzurch1k <galcenkovlad7@gmail.com>
2024-10-03 13:19:09 +0300

    fix

--- src/js/parts/map.js
+++ src/js/parts/map.js
@@ -4,27 +4,32 @@ import 'leaflet/dist/leaflet.css';
 // Знаходимо елемент карти
 const mapElement = document.getElementById('map');
 
-// Отримуємо значення широти, довготи і зуму з data-атрибутів
-const lat = mapElement.dataset.lat;
-const lng = mapElement.dataset.lng;
-const zoom = mapElement.dataset.zoom;
-const markerIcon = mapElement.dataset.marker;
+if (mapElement) {
+  // Отримуємо значення широти, довготи і зуму з data-атрибутів
+  const lat = mapElement.dataset.lat;
+  const lng = mapElement.dataset.lng;
+  const zoom = mapElement.dataset.zoom;
+  const markerIcon = mapElement.dataset.marker;
 
-// Ініціалізація карти з координатами та масштабом з data-атрибутів
-const map = L.map('map').setView([lat, lng], zoom);
+  // Ініціалізація карти з координатами та масштабом з data-атрибутів
+  const map = L.map('map').setView([lat, lng], zoom);
 
-// Додавання тайлів з OpenStreetMap
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  maxZoom: 19,
-}).addTo(map);
+  // Додавання тайлів з OpenStreetMap
+  L.tileLayer(
+    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    {
+      maxZoom: 19,
+    }
+  ).addTo(map);
 
-// Створення кастомної іконки для маркера
-const customIcon = L.icon({
-  iconUrl: markerIcon, // Посилання на твою іконку
-  iconSize: [26, 44], // Розмір іконки
-  iconAnchor: [13, 44], // Точка прив'язки іконки
-  popupAnchor: [0, -40], // Точка відкриття попапу (не використовується без попапу)
-});
+  // Створення кастомної іконки для маркера
+  const customIcon = L.icon({
+    iconUrl: markerIcon, // Посилання на твою іконку
+    iconSize: [26, 44], // Розмір іконки
+    iconAnchor: [13, 44], // Точка прив'язки іконки
+    popupAnchor: [0, -40], // Точка відкриття попапу (не використовується без попапу)
+  });
 
-// Додавання маркера з кастомною іконкою на карту без попапу
-const marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
+  // Додавання маркера з кастомною іконкою на карту без попапу
+  const marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
+}
